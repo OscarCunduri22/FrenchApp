bb4ef1b169a059e19f8658afbf74b2f2574d9ca0
OscarCunduri22 <oscar.cunduri@epn.edu.ec>
2024-06-04 17:52:16 -0500

    partial student list

--- lib/view/auth/student_list.dart
+++ lib/view/auth/student_list.dart
@@ -13,47 +13,99 @@ class StudentListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final databaseRepository = DatabaseRepository();
-    final List<Color> colors = [
-      Colors.red,
-      Colors.blue,
-      Colors.green,
-      Colors.orange,
-      Colors.purple,
-      Colors.pink,
-    ];
-    final Random random = Random();
+
+    void handleStudentTap(String studentId) {
+      // Navigate to the new view with the student ID
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PlaceholderScreen(
+              studentId: studentId), // Update with actual screen
+        ),
+      );
+    }
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lista de Estudiantes'),
-      ),
-      body: Container(
-        color: Colors.cyan, // Set a vibrant background color
-        child: FutureBuilder<List<Student>?>(
-          future: databaseRepository.getStudentsByTutorId(tutorId),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(child: Text('No hay estudiantes.'));
-            } else {
-              final students = snapshot.data!;
-              return GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 5,
+      body: Stack(
+        children: [
+          // Primary Background Image
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/auth/studentlist_bg.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Centered Secondary Background Container with transparency
+          Center(
+            child: FractionallySizedBox(
+              widthFactor: 0.8, // 80% of the screen width
+              heightFactor: 0.8, // 80% of the screen height
+              child: Container(
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/auth/book.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  color: Colors.white.withOpacity(0.9), // Add transparency
                 ),
-                itemCount: students.length,
-                itemBuilder: (context, index) {
-                  final color = colors[random.nextInt(colors.length)];
-                  return StudentCard(
-                      student: students[index], backgroundColor: color);
-                },
-              );
-            }
-          },
-        ),
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: databaseRepository.getStudentsByTutorId(tutorId),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(child: Text('No hay estudiantes.'));
+                    } else {
+                      final studentsDocs = snapshot.data!;
+                      return GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 5,
+                        ),
+                        itemCount: studentsDocs.length,
+                        itemBuilder: (context, index) {
+                          final studentData = studentsDocs[index]['data'];
+                          final studentId = studentsDocs[index]['id'];
+                          final student = Student.fromJson(studentData);
+                          return StudentCard(
+                            student: student,
+                            onTap: handleStudentTap,
+                            studentId: studentId,
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PlaceholderScreen extends StatelessWidget {
+  final String studentId;
+
+  const PlaceholderScreen({Key? key, required this.studentId})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student Details'),
+      ),
+      body: Center(
+        child: Text('Student ID: $studentId'),
       ),
     );
   }
