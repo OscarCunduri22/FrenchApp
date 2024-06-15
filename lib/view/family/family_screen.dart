d630c4faa918247e280d1a48f7a32800134a3a60
Nicolas <nicolastoscano1@hotmail.com>
2024-06-15 13:01:31 -0500

    familygame

--- lib/view/family/family_screen.dart
+++ lib/view/family/family_screen.dart
@@ -1,5 +1,23 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:frenc_app/utils/progress_bar.dart';
+import 'package:frenc_app/utils/replay_popup.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Family Game',
+      theme: ThemeData.light(useMaterial3: true),
+      home: const FamilyGame(),
+    );
+  }
+}
 
 class FamilyGame extends StatefulWidget {
   const FamilyGame({super.key});
@@ -10,7 +28,9 @@ class FamilyGame extends StatefulWidget {
 
 class _FamilyGameState extends State<FamilyGame> {
   late String cardUp;
-  late String cardsDown;
+  late List<String> cardsDown;
+  int score = 0;
+  bool roundCompleted = false;
 
   @override
   void initState() {
@@ -20,76 +40,124 @@ class _FamilyGameState extends State<FamilyGame> {
 
   void newGame() {
     List<String> images = [
-      'assets/images/brother_caucasian.png',
-      'assets/images/father_caucasian.png',
-      'assets/images/sister_caucasian.png',
-      'assets/images/mother_caucasian.png'
+      'assets/images/family/brother_caucasian.png',
+      'assets/images/family/father_caucasian.png',
+      'assets/images/family/sister_caucasian.png',
+      'assets/images/family/mother_caucasian.png'
     ];
 
     cardUp = images[Random().nextInt(images.length)];
 
-    cardsDown = images[Random().nextInt(images.length)];
+    // Ensure all cards are different and only one matches the cardUp
+    cardsDown = images.where((image) => image != cardUp).toList();
+    cardsDown.shuffle();
+    cardsDown = cardsDown.take(3).toList();
+    cardsDown.add(cardUp);
+    cardsDown.shuffle();
 
     setState(() {});
   }
 
+  void checkMatch(String selectedCard) {
+    if (selectedCard == cardUp) {
+      setState(() {
+        score++;
+        if (score >= 10) {
+          _showWinDialog();
+        } else {
+          newGame();
+        }
+      });
+    }
+  }
+
+  void _showWinDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => ReplayPopup(
+        score: score,
+        onReplay: () {
+          setState(() {
+            score = 0;
+            newGame();
+          });
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    const Color backgroundColor = Color.fromARGB(255, 118, 207, 191);
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Family Game'),
-        backgroundColor: backgroundColor,
-        toolbarHeight: 40,
-      ),
       body: Container(
         decoration: const BoxDecoration(
-          color: backgroundColor,
+          color: Color.fromARGB(255, 118, 207, 191),
         ),
-        // Code for the card goes up
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 100,
-              height: 150,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(cardUp),
-                  fit: BoxFit.cover,
+            ProgressBar(
+              backgroundColor: const Color(0xFF424141),
+              progressBarColor: const Color(0xFF8DB270),
+              headerText:
+                  'Sélectionnez l\'image qui ressemble à celle ci-dessus',
+              progressValue: score / 10,
+              onBack: () {
+                // Acción para retroceder
+              },
+              onVolume: () {
+                // Acción para controlar el volumen
+              },
+            ),
+            const SizedBox(height: 20),
+            BounceInDown(
+              key: UniqueKey(), // Unique key to force rebuild
+              child: Container(
+                width: 90,
+                height: 110,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(cardUp),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(15.0),
                 ),
-                borderRadius: BorderRadius.circular(15.0),
               ),
             ),
             const SizedBox(height: 20),
-            // Code for the cards goes down
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                for (String image in [
-                  'assets/images/brother_caucasian.png',
-                  'assets/images/father_caucasian.png',
-                  'assets/images/sister_caucasian.png',
-                  'assets/images/mother_caucasian.png'
-                ])
-                  GestureDetector(
-                    child: Container(
-                      width: 100,
-                      height: 150,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(image),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                    ),
-                  ),
-              ],
+              children: cardsDown.asMap().entries.map((entry) {
+                int index = entry.key;
+                String image = entry.value;
+                Widget animatedImage;
+
+                animatedImage = BounceInUp(
+                  key: UniqueKey(),
+                  child: buildImageCard(image),
+                );
+
+                return GestureDetector(
+                  onTap: () => checkMatch(image),
+                  child: animatedImage,
+                );
+              }).toList(),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildImageCard(String image) {
+    return Container(
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(15.0),
       ),
     );
   }
