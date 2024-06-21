e87552cebdcba0a3558e970b4a73b97ba7452dff
OscarCunduri22 <oscar.cunduri@epn.edu.ec>
2024-06-21 12:40:11 -0500

    merged from main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frenc_app/firebase_options.dart';
 import 'package:frenc_app/utils/user_provider.dart';
-import 'package:frenc_app/view/family/game3/drag_family.dart';
 import 'package:frenc_app/view/numbers/game3/game_screen.dart';
 import 'package:frenc_app/view/start_screen.dart';
 import 'package:provider/provider.dart';
